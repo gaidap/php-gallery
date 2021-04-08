596c47337e8cc6e7438eea1e1f43ce587f73fb46
pgaida <p.gaida@omikronsystems.de>
2021-04-08 17:15:40 +0200

    First refactorings and updated code style

--- index.php
+++ index.php
@@ -1,30 +1,19 @@
 <?php include("includes/header.php"); ?>
 
 
-        <div class="row">
+<div class="row">
 
-            <!-- Blog Entries Column -->
-            <div class="col-md-8">
+    <!-- Blog Entries Column -->
+    <div class="col-md-8">
 
+
+    </div>
+
+
+    <!-- Blog Sidebar Widgets Column -->
+    <div class="col-md-4">
+        <?php include("includes/sidebar.php"); ?>
+    </div>
+    <!-- /.row -->
     
-            
-          
-         
-
-            </div>
-
-
-
-
-            <!-- Blog Sidebar Widgets Column -->
-            <div class="col-md-4">
-
-            
-                 <?php include("includes/sidebar.php"); ?>
-
-
-
-        </div>
-        <!-- /.row -->
-
-        <?php include("includes/footer.php"); ?>
+    <?php include("includes/footer.php"); ?>
