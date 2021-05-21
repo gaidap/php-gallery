65ef98ffbdcab0178ad19a3d8b89bdc5bd76e763
pgaida <p.gaida@omikronsystems.de>
2021-05-21 22:31:55 +0200

    Updated Photos Table.

--- schema/photos.sql
+++ schema/photos.sql
@@ -1,15 +1,18 @@
 -- auto-generated definition
 create table photos
 (
-    id            int auto_increment
+    id             int auto_increment
         primary key,
-    file_name     varchar(255)                          not null,
-    title         varchar(255)                          not null,
-    type          varchar(255)                          not null,
-    size          int                                   not null,
-    description   text                                  null,
-    creation_date timestamp default current_timestamp() not null,
-    constraint photos_file_name_uindex
-        unique (file_name)
+    file           varchar(255)                             not null,
+    file_name      varchar(255)                             not null,
+    title          varchar(255)                             not null,
+    caption        varchar(255)                             null,
+    alternate_text varchar(255) default ''                  not null,
+    type           varchar(255)                             not null,
+    size           int                                      not null,
+    description    text                                     null,
+    creation_date  timestamp    default current_timestamp() not null,
+    constraint photos_file_path_uindex
+        unique (file)
 );
 
